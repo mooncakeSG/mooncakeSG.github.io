0aae399ce8633db90b7744f2b4edf1d3cf7eb722
mooncakeSG <keawinkoesnel804@gmail.com>
2025-04-03 19:59:35 +0200

    Hamburger & removed contact on foooter

--- script.js
+++ script.js
@@ -1373,4 +1373,28 @@ document.addEventListener('DOMContentLoaded', () => {
     sections.forEach(section => {
         sectionObserver.observe(section);
     });
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach(el => {
+        el.addEventListener('click', () => {
+            // Get the target from the "data-target" attribute
+            const target = el.dataset.target;
+            const $target = document.getElementById(target);
+
+            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+            el.classList.toggle('is-active');
+            document.querySelector('.navbar-menu').classList.toggle('is-active');
+        });
+    });
+
+    // Close mobile menu when clicking a link
+    document.querySelectorAll('.navbar-item').forEach(item => {
+        item.addEventListener('click', () => {
+            document.querySelector('.navbar-burger').classList.remove('is-active');
+            document.querySelector('.navbar-menu').classList.remove('is-active');
+        });
+    });
 }); 
